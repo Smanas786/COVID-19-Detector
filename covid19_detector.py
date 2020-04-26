c570c5e70996b92cda28ae5f817ab7b64b1cf7d0
Smanas786 <42001169+Smanas786@users.noreply.github.com>
2020-04-26 14:05:27 -0400

    covid19_detector Model
    Model created using X-ray images of COVID-19 patients

---
+++ covid19_detector.py
@@ -0,0 +1,203 @@
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.layers import AveragePooling2D
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, roc_auc_score, auc
+from sklearn.metrics import confusion_matrix
+from imutils import paths
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import cv2
+import os
+
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix, roc_curve
+import itertools
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.figure(figsize = (5,5))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=90)
+    plt.yticks(tick_marks, classes)
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+INIT_LR = 1e-3
+EPOCHS = 32
+BS = 8
+
+print("[INFO] loading images...")
+imagePaths = list(paths.list_images(".\Latest"))
+data = []
+labels = []
+
+# loop over the image paths
+for imagePath in imagePaths:
+	# extract the class label from the filename
+	label = imagePath.split(os.path.sep)[-2]
+
+	# load the image, swap color channels, and resize it to be a fixed
+	# 224x224 pixels while ignoring aspect ratio
+	image = cv2.imread(imagePath)
+	image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+	image = cv2.resize(image, (224, 224))
+
+	# update the data and labels lists, respectively
+	data.append(image)
+	labels.append(label)
+
+data = np.array(data) / 255.0
+labels = np.array(labels)
+
+# perform one-hot encoding on the labels - optional
+lb = LabelBinarizer()
+labels = lb.fit_transform(labels)
+labels = to_categorical(labels)
+
+# partition 80-20 ratio
+(trainX, testX, trainY, testY) = train_test_split(data, labels,
+	test_size=0.20, stratify=labels, random_state=42)
+
+# initialize the training data augmentation object
+trainAug = ImageDataGenerator(
+	rotation_range=15,
+       zca_whitening=False,  # apply ZCA whitening
+       width_shift_range=0.2,  # randomly shift images horizontally (fraction of total width)
+       height_shift_range=0.2,  # randomly shift images vertically (fraction of total height)
+       horizontal_flip=True,  # randomly flip images
+       vertical_flip=True,    # randomly flip images
+       brightness_range=[0.2,1.0],
+       zoom_range=[0.5,1.0],
+       fill_mode="nearest"
+    )
+
+# load the VGG16 network
+baseModel = VGG16(weights="imagenet", include_top=False,
+	input_tensor=Input(shape=(224, 224, 3)))
+
+# construct the head of the model that will be placed on top of the
+# the base model
+headModel = baseModel.output
+headModel = AveragePooling2D(pool_size=(4, 4))(headModel)
+headModel = Flatten(name="flatten")(headModel)
+headModel = Dense(64, activation="relu")(headModel)
+headModel = Dropout(0.5)(headModel)
+headModel = Dense(2, activation="softmax")(headModel)
+
+# fine tuning steps for head placment on top of model
+model = Model(inputs=baseModel.input, outputs=headModel)
+
+# fine tuning step - freezing rest of the layers below head
+for layer in baseModel.layers:
+	layer.trainable = False
+
+# compile
+print("[INFO] compiling model...")
+opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
+model.compile(loss="binary_crossentropy", optimizer=opt,
+	metrics=["accuracy"])
+
+# head
+print("[INFO] training head...")
+H = model.fit(
+	trainAug.flow(trainX, trainY, batch_size=BS),
+	steps_per_epoch=len(trainX) // BS,
+	validation_data=(testX, testY),
+	validation_steps=len(testX) // BS,
+	epochs=EPOCHS)
+
+# make predictions on the testing set
+print("[INFO] evaluating network...")
+predIdxs = model.predict(testX, batch_size=BS)
+
+# for each image in the testing set we need to find the index of the
+# label with corresponding largest predicted probability
+predIdxs = np.argmax(predIdxs, axis=1)
+
+# show a nicely formatted classification report
+print(classification_report(testY.argmax(axis=1), predIdxs,
+	target_names=lb.classes_))
+
+# compute the confusion matrix and and use it to derive the raw
+# accuracy, sensitivity, and specificity
+cm = confusion_matrix(testY.argmax(axis=1), predIdxs)
+total = sum(sum(cm))
+acc = (cm[0, 0] + cm[1, 1]) / total
+sensitivity = cm[0, 0] / (cm[0, 0] + cm[0, 1])
+specificity = cm[1, 1] / (cm[1, 0] + cm[1, 1])
+
+confusion_matrixx = metrics.confusion_matrix(y_true=testY.argmax(axis=1), y_pred=predIdxs)
+print(confusion_matrixx)
+dict_characters = {0: 'IDC(-)', 1: 'IDC(+)'}
+plot_confusion_matrix(cm, classes = list(dict_characters.values())) 
+plt.show()
+plt.close()
+# show the confusion matrix, accuracy, sensitivity, and specificity
+print(cm)
+print("acc: {:.4f}".format(acc))
+print("sensitivity: {:.4f}".format(sensitivity))
+print("specificity: {:.4f}".format(specificity))
+
+# plot the training loss and accuracy
+N = EPOCHS
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, N), H.history["accuracy"], label="train_acc")
+plt.plot(np.arange(0, N), H.history["val_accuracy"], label="val_acc")
+plt.title("Training Loss and Accuracy on COVID-19 Dataset")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend(loc="lower left")
+#plt.savefig("plot")
+plt.close()
+tg_names=['ID-','ID+']
+report = metrics.classification_report(testY.argmax(axis=1), predIdxs, target_names=tg_names)
+print(report)  
+
+roc_log = roc_auc_score(testY.argmax(axis=1), predIdxs)
+false_positive_rate, true_positive_rate, threshold = roc_curve(testY.argmax(axis=1), predIdxs)
+area_under_curve = auc(false_positive_rate, true_positive_rate)
+
+plt.plot([0, 1], [0, 1], 'r--')
+plt.plot(false_positive_rate, true_positive_rate, label='AUC = {:.3f}'.format(area_under_curve))
+plt.xlabel('False positive rate')
+plt.ylabel('True positive rate')
+plt.title('ROC curve')
+plt.legend(loc='best')
+plt.show()
+#plt.savefig(ROC_PLOT_FILE, bbox_inches='tight')
+plt.close()
+
+print(np.mean(H.history["loss"]))
+print(np.mean(H.history["val_loss"]))
